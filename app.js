4b627c3c72033094141445137603b4a8e7948a41
joseterrera <joseterrera@yahoo.com>
2015-08-26 15:52:43 -0500

    add comments

--- app.js
+++ app.js
@@ -1,45 +1,77 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
 app.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider){
-		$stateProvider
-			.state('home', {
-				url: '/home',
-				templateUrl: '/home.html',
-				controller: 'MainCtrl'
-			});
-			//if router receives a url not defined, it will go home.
-			$urlRouterProvider.otherwise('home');
-		}]);
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url:'/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+        //if router receives a url not defined, it will go home.
+        $urlRouterProvider.otherwise('home');
+    }
+]);
 
-app.factory('posts', [function(){
-	var o = {
-		posts: [{title: 'hello', link: '', upvotes: 0}]
-	};
-	return o;
-	// o can now be injected in any module.
+app.factory('posts', [function() {
+    var o = {
+        posts: [{
+            title: 'hello',
+            link: '',
+            upvotes: 0
+        }]
+    };
+    return o;
+    // o can now be injected in any module.
 }]);
 
 app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts) {
+        $scope.posts = posts.posts;
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') {
+                return;
+            }
+            $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0,
+            comments: [
+              {author: 'Joe', body: 'Cool post!', upvotes: 0},
+              {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+            ]
+          });
+            $scope.title = '';
+            $scope.link = '';
+        };
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+    }
+]);
+
+app.controller('PostsCtrl', [
 '$scope',
+'$stateParams',
 'posts',
-function($scope, posts){
-  	$scope.posts = posts.posts;
-		$scope.addPost = function(){
-	  if(!$scope.title || $scope.title === '') { return; }
-	  $scope.posts.push({
-	    title: $scope.title,
-	    link: $scope.link,
-	    upvotes: 0
-	  });
-	  $scope.title = '';
-	  $scope.link = '';
-	};
-	$scope.incrementUpvotes = function(post){
-		post.upvotes +=1;
-	};
+function($scope, $stateParams, posts){
+     $scope.post = posts.posts[$stateParams.id];
+     $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
 }]);
-
-
